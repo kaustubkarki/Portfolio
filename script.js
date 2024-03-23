6e5df40b25a002f0746f0b16740aed05c9b45b22
Kaustub karki <kaustubkarki@gmail.com>
2024-03-23 16:01:12 +0545

    page1 compelete

---
+++ script.js
@@ -0,0 +1,34 @@
+function videoPlAY() {
+  const video = document.querySelector("#slider3 #vid-div video");
+
+  video.addEventListener("mouseenter", () => {
+    video.play();
+  });
+  video.addEventListener("mouseleave", () => {
+    video.pause();
+  });
+}
+videoPlAY();
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+var tl = gsap.timeline();
+
+tl.to("#page1", {
+  y: "100vh",
+  scale: 0.6,
+  duration: 0,
+});
+tl.to("#page1", {
+  y: "30vh",
+  duration: 1,
+  delay: 1,
+});
+tl.to("#page1", {
+  y: "0vh",
+  rotate: 360,
+  scale: 1,
+  duration: 0.8,
+});
